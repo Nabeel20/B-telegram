09e213612598edee91e00f6d3a32f09493ce2e9e
Nabeel20 <nabeelandnizam@gmail.com>
2021-04-24 15:11:52 +0300

    0.2

--- api/brain.js
+++ api/brain.js
@@ -3,9 +3,7 @@ module.exports = (req, res) => {
     const { body } = req;
     const { chat: { id }, text } = body.message;
     const bot = new TeleBot('1785552676:AAFICB4xRoNHcK0Ve-lbXCbSePtfwwsanfo');
-    bot.on('/start', () => {
-        return bot.sendMessage('186274711', 'Welcome :)');
-    });
+    bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome  !!'));
     bot.start();
     response.send('OK');
 }
--- api/main.js
+++ api/main.js
@@ -1,9 +1,9 @@
-const TeleBot = require('telebot');
-const bot = new TeleBot('1785552676:AAFICB4xRoNHcK0Ve-lbXCbSePtfwwsanfo');
+import { Telegraf } from 'telegraf';
+const bot = new Telegraf('1785552676:AAFICB4xRoNHcK0Ve-lbXCbSePtfwwsanfo');
+
 module.exports = (req, res) => {
     const { body } = req;
     const { chat: { id }, text } = body.message;
-    console.log(message)
-    bot.sendMessage(chat.id, text)
+    bot.hears('hi', (ctx) => ctx.reply('Hey there'))
     bot.start();
 }
