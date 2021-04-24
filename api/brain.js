7fddd69d47a341771f67c2be42506a1f2c9ab2a3
Nabeel20 <nabeelandnizam@gmail.com>
2021-04-24 21:15:02 +0300

    4.0.3

--- api/brain.js
+++ api/brain.js
@@ -1,7 +1,14 @@
 const token = '1785552676:AAFICB4xRoNHcK0Ve-lbXCbSePtfwwsanfo';
 const { Telegraf } = require('telegraf');
 const bot = new Telegraf(token);
-
-bot.start((ctx) => ctx.reply('Welcome'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 bot.launch();
+
+
+module.exports = (req, res) => {
+    const { body } = req;
+
+    bot.start((ctx) => ctx.reply('Welcome'));
+    bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+    bot.hears('body', ctx => ctx.reply(`body is: ${JSON.stringify(body)}`))
+    res.status(200).send(`Bot is alive!`)
+}
