ede7fef1315445db40235f65b993ca85b858d376
Nabeel20 <nabeelandnizam@gmail.com>
2021-04-24 18:30:26 +0300

    2.10.5

--- api/brain.js
+++ api/brain.js
@@ -1,15 +1,21 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = '1785552676:AAFICB4xRoNHcK0Ve-lbXCbSePtfwwsanfo';
 const bot = new TelegramBot(token, { polling: true });
+
+const TeleBot = require('telebot');
+const tele_bot = new TeleBot('1785552676:AAFICB4xRoNHcK0Ve-lbXCbSePtfwwsanfo');
+
 module.exports = (req, res) => {
     try {
         const { body } = req;
         bot.on('message', (msg) => {
-            const chatId = msg.chat.id;
-
             // send a message to the chat acknowledging receipt of their message
             bot.sendMessage(186274711, 'Received your message');
         });
+        tele_bot.on(['/start', 'audio', 'sticker'], msg => {
+            return bot.sendMessage(186274711, 'Bam!');
+        });
+        tele_bot.start();
     } catch (error) {
 
     }
