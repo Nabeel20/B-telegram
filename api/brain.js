eadbc3f169ec211244940313674282288928abc2
Nabeel20 <nabeelandnizam@gmail.com>
2021-04-24 14:16:53 +0300

    add brain to api :bulb:

---
+++ api/brain.js
@@ -0,0 +1,9 @@
+const TeleBot = require('telebot');
+const bot = new TeleBot('1785552676:AAFICB4xRoNHcK0Ve-lbXCbSePtfwwsanfo');
+module.exports = (req, res) => {
+    const { body } = req;
+    const { chat: { id }, text } = body.message;
+    console.log(message)
+    bot.sendMessage(chat.id, text)
+    bot.start();
+}
