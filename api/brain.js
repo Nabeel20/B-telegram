daae03b2fbbc33424e7e77da619169636655c127
Nabeel20 <nabeelandnizam@gmail.com>
2021-04-24 20:22:46 +0300

    3.0

--- api/brain.js
+++ api/brain.js
@@ -1,18 +1,20 @@
-const TelegramBot = require('node-telegram-bot-api');
+//const TelegramBot = require('node-telegram-bot-api');
 const token = '1785552676:AAFICB4xRoNHcK0Ve-lbXCbSePtfwwsanfo';
-const bot = new TelegramBot(token, { polling: true });
+//const bot = new TelegramBot(token, { polling: true });
+const TeleBot = require('telebot');
+const tele_bot = new TeleBot({
+    token,
+    webhook: {
+        url: 'b-t-b-6t5c0mbxi-nabeel20.vercel.app/api/brain', // HTTPS url to send updates to.
+    },
+});
+tele_bot.start();
 
+tele_bot.on('text', (msg) => msg.reply.text(msg.text))
 module.exports = (req, res) => {
     try {
-        const { body } = req;
-        const TeleBot = require('telebot');
-        const tele_bot = new TeleBot('1785552676:AAFICB4xRoNHcK0Ve-lbXCbSePtfwwsanfo');
-        tele_bot.on(['/start', 'audio', 'sticker'], msg => {
-            return bot.sendMessage(186274711, 'Bam!');
-        });
-        tele_bot.start();
+        console.log('0.4');
     } catch (error) {
-
+        console.log('g')
     }
-
 }
