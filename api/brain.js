9f5ad5d4858389b99386d4758aa342fb44336ed5
Nabeel20 <nabeelandnizam@gmail.com>
2021-04-24 18:32:32 +0300

    2.10.6

--- api/brain.js
+++ api/brain.js
@@ -8,10 +8,6 @@ const tele_bot = new TeleBot('1785552676:AAFICB4xRoNHcK0Ve-lbXCbSePtfwwsanfo');
 module.exports = (req, res) => {
     try {
         const { body } = req;
-        bot.on('message', (msg) => {
-            // send a message to the chat acknowledging receipt of their message
-            bot.sendMessage(186274711, 'Received your message');
-        });
         tele_bot.on(['/start', 'audio', 'sticker'], msg => {
             return bot.sendMessage(186274711, 'Bam!');
         });
