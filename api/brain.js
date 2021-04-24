2273394ea3859ec5c2f6f91aca58c4b754ed7c46
Nabeel20 <nabeelandnizam@gmail.com>
2021-04-24 21:01:26 +0300

    4.0.1

--- api/brain.js
+++ api/brain.js
@@ -2,4 +2,5 @@ const token = '1785552676:AAFICB4xRoNHcK0Ve-lbXCbSePtfwwsanfo';
 const bot = new Telegraf(token);
 
 bot.start((ctx) => ctx.reply('Welcome'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 bot.launch();
