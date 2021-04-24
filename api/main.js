dafb179c58c1fab7665828c33fd5e060387a64c6
Nabeel20 <nabeelandnizam@gmail.com>
2021-04-24 10:04:50 +0300

    adding logic 0.1

---
+++ api/main.js
@@ -0,0 +1,8 @@
+import { Telegraf } from 'telegraf';
+
+const bot = new Telegraf(process.env.Bot_token);
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
