9aabdea157350d3d0b5eadd300b77fffe414f8ea
Nabeel20 <nabeelandnizam@gmail.com>
2021-04-25 10:53:25 +0300

    6.4

--- api/brain.js
+++ api/brain.js
@@ -1,23 +1,30 @@
 const { Telegraf } = require('telegraf');
+const { json } = require('micro');
 
 const token = '1785552676:AAFICB4xRoNHcK0Ve-lbXCbSePtfwwsanfo';
 const bot = new Telegraf(token, { telegram: { webhookReply: false } });
 
 bot.command('hello', (ctx) => ctx.reply('Hello, friend!'));
-const { json } = require('micro');
+bot.start((ctx) => ctx.reply(`أهلاً ${ctx.first_name}`));
+bot.on('text', ctx => {
+    ctx.reply(handel_text_input(ctx.text))
+})
 
 module.exports = async function (req, res) {
     try {
         const body = await json(req)
-        console.log(body)
         bot.handleUpdate(body)
         res.statusCode = 200
         res.end('')
     } catch (e) {
         res.statusCode = 500
         res.setHeader('Content-Type', 'text/html')
-        res.end('<h1>Server Error</h1><p>Sorry, there was a problem</p>')
-        console.error(e.message)
+        res.end('<h1>Server Error</h1><p>عذراً، حدثت مشكلة</p>')
     }
 };
 
+function handel_text_input(input_text) {
+    let names = ['Nabeel', 'Najwa', 'Adnan'];
+    if (names.includes(input_text)) return 'Welcome! Family ;)'
+    return 'Welcome - not family'
+}
